47cf00b9f6a89cd75ceed54ca96f1d7d5ba4c40e
Pascal P <lacsap_666@yahoo.fr>
2020-08-30 21:56:25 +1200

    blog app - iter 1

---
+++ mysite/blog/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+
+# A model manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+            .filter(status='published')
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+
+    # 1-to-many relationshup Post ---> User(Author)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    body = models.TextField(max_length=16_384)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    #
+    objects = models.Manager()     ## The default manager.
+    published = PublishedManager() ## Our custom manager.
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
