24636132f5bfe6dbf2a0306a19bc433fea641f25
Pascal P <lacsap_666@yahoo.fr>
2020-09-02 11:10:11 +1200

    iter7: search using 3-gram similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from django.core.mail import send_mail
 from django.db.models import Count
 from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -114,14 +114,9 @@ def post_search(request):
         if form.is_valid():
             query = form.cleaned_data['query']
 
-            search_vector = SearchVector('title', weight='A') + \
-                SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
-
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')
+               similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request, 'blog/post/search.html',
                   {'form': form, 'query': query, 'results': results})
