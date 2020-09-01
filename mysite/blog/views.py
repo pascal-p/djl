63d9bea3441e80b22a1962790adf6f63f1cc98a3
Pascal P <lacsap_666@yahoo.fr>
2020-09-02 11:07:30 +1200

    iter6: try stemming and ranking

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from django.core.mail import send_mail
 from django.db.models import Count
 from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -113,9 +113,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+
+            search_vector = SearchVector('title', weight='A') + \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
 
     return render(request, 'blog/post/search.html',
                   {'form': form, 'query': query, 'results': results})
