303a7a3887fcfcaa58025e3d87ddcf7d929dae18
Pascal P <lacsap_666@yahoo.fr>
2020-09-04 11:35:52 +1200

    iter6: add ajax action using jquery

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -26,6 +28,23 @@ def image_create(request):
 
     return render(request, 'images/image/create.html',
                   {'section': 'images', 'form': form})
+
+@login_required
+@require_POST
+def image_like(request):
+    img_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if img_id and action:
+        try:
+            img = Image.objects.get(id=img_id)
+            if action == 'like':
+                img.users_like.add(request.user)
+            else:
+                img.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'error'})
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
