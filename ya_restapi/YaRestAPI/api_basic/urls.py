05f5cd5122e5b3e7cc6db856a6e799ebe16c7a73
Pascal P <lacsap_666@yahoo.fr>
2021-01-07 22:10:09 +1300

    ViewSets and Routers

--- ya_restapi/YaRestAPI/api_basic/urls.py
+++ ya_restapi/YaRestAPI/api_basic/urls.py
@@ -1,10 +1,22 @@
-from django.urls import path
-from .views import ArticleAPIView, ArticleDetailAPIView, GenericAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ArticleAPIView, ArticleDetailAPIView, \
+  GenericAPIView, ArticleViewSet
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
+
 
 urlpatterns = [
-    path('article/', ArticleAPIView.as_view()),
+    # ArticleViewSet
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>', include(router.urls)),
 
+    # APIView
+    path('article/', ArticleAPIView.as_view()),
     path('article-detail/<int:id>', ArticleDetailAPIView.as_view()),
 
+    # GenericAPIView
     path('generic/article/<int:id>', GenericAPIView.as_view()),
 ]
--- ya_restapi/YaRestAPI/api_basic/views.py
+++ ya_restapi/YaRestAPI/api_basic/views.py
@@ -7,14 +7,51 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework  import status
 from rest_framework.views  import APIView
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, viewsets
 # Authentication
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+
 
 from .models import Article
 from .serializers import ArticleSerializer
 
+
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def partial_update(self, request, pk=None):
+    #    pass
+
+    def destroy(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 class GenericAPIView(generics.GenericAPIView,
                      mixins.ListModelMixin, mixins.CreateModelMixin,
