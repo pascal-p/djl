6ae038f0587914171ee040f9d1e574a67470e0b9
Pascal P <lacsap_666@yahoo.fr>
2021-01-07 15:55:53 +1300

    api_views() decorator

--- ya_restapi/YaRestAPI/api_basic/serializers.py
+++ ya_restapi/YaRestAPI/api_basic/serializers.py
@@ -4,4 +4,4 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'email']
+        fields = ['id', 'title', 'author', 'email']
