1db871017779ab8f78aa72ca6f366318da4f0769
Pascal P <lacsap_666@yahoo.fr>
2021-01-07 17:01:14 +1300

    generic views

--- ya_restapi/YaRestAPI/api_basic/urls.py
+++ ya_restapi/YaRestAPI/api_basic/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import ArticleAPIView, ArticleDetailAPIView
-# from .views import article_list, article_detail,
+from .views import ArticleAPIView, ArticleDetailAPIView, GenericAPIView
 
 urlpatterns = [
-    # path('article/', article_list),
     path('article/', ArticleAPIView.as_view()),
 
-    # path('article-detail/<int:pk>', article_detail),
     path('article-detail/<int:id>', ArticleDetailAPIView.as_view()),
+
+    path('generic/article/<int:id>', GenericAPIView.as_view()),
 ]
--- ya_restapi/YaRestAPI/api_basic/views.py
+++ ya_restapi/YaRestAPI/api_basic/views.py
@@ -7,10 +7,35 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework  import status
 from rest_framework.views  import APIView
-# from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics, mixins
 
 from .models import Article
 from .serializers import ArticleSerializer
+
+
+class GenericAPIView(generics.GenericAPIView,
+                     mixins.ListModelMixin, mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin, mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin):
+
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
 
 
 class ArticleAPIView(APIView):
