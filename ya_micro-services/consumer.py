ac8700674534961278aa16395ce03ad4eae6ec5b
Pascal P <lacsap_666@yahoo.fr>
2021-01-09 21:33:40 +1300

    add consumer/producer to django side

---
+++ ya_micro-services/consumer.py
@@ -0,0 +1,32 @@
+import pika, json, os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ya_ms_admin.settings")
+django.setup()
+
+# with two lines above, we can now import Product
+from ya_products.models import Product
+
+## root (Django) project - consuming from 'admin_q' queue
+params = pika.URLParameters('amqp://corto:B@merSalEe@host.docker.internal.localhost:5672/')
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.queue_declare(queue='admin_q', durable=True)
+
+
+def callback(ch, method, properties, body):
+    print('Received in admin')
+    id = json.loads(body)
+    print(f' id: {id}')
+    product = Product.objects.get(id=id)
+    product.likes += 1
+    product.save()
+    print(f' Product likes increased! => {product.likes}')
+    return
+
+channel.basic_consume(queue='admin_q', on_message_callback=callback,
+                      auto_ack=True)
+print('Started Consuming / (root) Admin side...')
+channel.start_consuming()
+
+channel.close()
