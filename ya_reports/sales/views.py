257d8cb3ed13b26d8bfb04abad75c9d296ffab7a
Pascal P <lacsap_666@yahoo.fr>
2021-05-15 20:30:52 +1200

    first view and template

--- ya_reports/sales/views.py
+++ ya_reports/sales/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    ## function views
+    hw = "hello world from the view"
+    return render(request, 'sales/main.html', {'h': hw})
