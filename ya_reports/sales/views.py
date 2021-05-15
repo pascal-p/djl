41c4ca51335e714841c47f48a1a893fdc201fb65
Pascal P <lacsap_666@yahoo.fr>
2021-05-16 07:45:43 +1200

    add dataframe for the positions

--- ya_reports/sales/views.py
+++ ya_reports/sales/views.py
@@ -10,26 +10,33 @@ from .forms import SalesSearchForm
 def home_view(request):
     ## function views
     form = SalesSearchForm(request.POST or None)
-    sales_df = None
+    sales_df, position_df = None, None
 
     if request.method == 'POST':
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
 
-        qs = Sale.objects.filter(created_at__date__lte=date_to,  created_at__date__gte=date_from) # queryset
-        if len(qs) > 0:
-            print(" ##### ")
-            sales_df = pd.DataFrame(qs.values())
-            print(sales_df)
-            sales_df = sales_df.to_html()
-            print(" ##### ")
+        sales_qs = Sale.objects.filter(created_at__date__lte=date_to,
+                                      created_at__date__gte=date_from) # queryset
+        if len(sales_qs) > 0:
+            sales_df = pd.DataFrame(sales_qs.values()).to_html()
+            position_data = [
+                {
+                    'position_id': pos.id,
+                    'product': pos.product.name,
+                    'quantity': pos.quantity,
+                    'price': pos.price
+                } for sale in sales_qs for pos in sale.get_positions()
+            ]
+            position_df = pd.DataFrame(position_data).to_html()
         else:
             print("No data")
 
     ctxt = {
         'form': form,
-        'sales_df': sales_df
+        'sales_df': sales_df,
+        'position_df': position_df,
     }
     return render(request, 'sales/home.html', ctxt)
 
