1d1d665f6cc3c91e90f02442ce5f1e6ff7285d60
Pascal P <lacsap_666@yahoo.fr>
2021-05-15 12:07:08 +1200

    Oooops settings and secret key

--- ya_reports/ya_reports/settings.py
+++ ya_reports/ya_reports/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x0*=9rx1!q=2m^_(77l%@3w!p=coo_6yh#owku%qyb&-j@&*h('
+SECRET_KEY = os.getenv("DJANGO_YAREPORT_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # our apps here:
+    'customers',
+    'products',
+    'profiles',
+    'reports',
+    'sales'
+
+    # 3rd party
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -49,12 +60,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 ROOT_URLCONF = 'ya_reports.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/'templates',
+]
+
+MEDIA_URL = '/media'
+
+MEDIA_ROOT = BASE_DIR/'media'
