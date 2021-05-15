9b12dab82668292c3f7134e7d0535a8ab4eb7cde
Pascal P <lacsap_666@yahoo.fr>
2021-05-15 20:46:11 +1200

    sales list (view)

--- ya_reports/sales/urls.py
+++ ya_reports/sales/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_view
-
+from .views import (
+    home_view,
+    SaleListView,
+)
 app_name = 'sales'
 
 urlpatterns = [
     path('', home_view, name='home'), # the main view hence ''
+    path('list/', SaleListView.as_view(), name='list'), 
 ]
--- ya_reports/sales/views.py
+++ ya_reports/sales/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Sale
+
 
 # Create your views here.
 def home_view(request):
     ## function views
     hw = "hello world from the view"
-    return render(request, 'sales/main.html', {'h': hw})
+    return render(request, 'sales/home.html', {'h': hw})
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
+    # context_object_name = 'qs' would replace object_list in the view main.html
