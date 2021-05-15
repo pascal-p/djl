1d49975aea1a4af196b02426596f4a31cec78cb0
Pascal P <lacsap_666@yahoo.fr>
2021-05-15 20:13:42 +1200

    report model

--- ya_reports/reports/models.py
+++ ya_reports/reports/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from profiles.models import Profile
+
+class Report(models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='reports', blank=True)
+    remarks = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
