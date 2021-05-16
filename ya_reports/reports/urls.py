978e3639d4a5b8cd3567c5c2f37b48a757d8e5b9
Pascal P <lacsap_666@yahoo.fr>
2021-05-16 15:57:18 +1200

    pdf report

--- ya_reports/reports/urls.py
+++ ya_reports/reports/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     create_report_view,
     ReportListView,
-    ReportDetailView
+    ReportDetailView,
+    render_pdf_view
 )
 
 app_name = 'reports'
@@ -11,4 +12,5 @@ urlpatterns = [
     path('', ReportListView.as_view(), name='main'),
     path('save/', create_report_view, name='create-report'),
     path('<pk>', ReportDetailView.as_view(), name='detail'),
+    path('<pk>/pdf/', render_pdf_view, name='pdf'),
 ]
--- ya_reports/reports/views.py
+++ ya_reports/reports/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.views.generic import ListView, DetailView
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
 from profiles.models import Profile
 from .utils import get_report_image
 from .models import Report
@@ -26,3 +32,21 @@ def create_report_view(request):
         return JsonResponse({'msg': 'Send'})
     #
     return JsonResponse({})
+
+def render_pdf_view(request, pk):
+    template_path = 'reports/pdf.html'
+    obj = get_object_or_404(Report, pk=pk)
+    context = {'obj': obj}
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="report.pdf"'
+
+    template = get_template(template_path)
+    html = template.render(context)
+
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
