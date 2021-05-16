e2b5a74e1707fb2726087984b8ee0b4cc016440b
Pascal P <lacsap_666@yahoo.fr>
2021-05-16 12:05:03 +1200

    create report object

--- ya_reports/sales/static/sales/home.js
+++ ya_reports/sales/static/sales/home.js
@@ -1,9 +1,15 @@
 const reportBtn = document.getElementById('report-btn');
 const img = document.getElementById('img');
 const modalBody = document.getElementById('modal-body');
+const reportForm = document.getElementById('report-form');
 
-console.log(reportBtn);
-console.log(img);
+const reportName = document.getElementById('id_name');
+const reportRemarks = document.getElementById('id_remarks');
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+
+// console.log(reportBtn);
+// console.log(img);
+// console.log(csrf);
 
 if (img) {
   reportBtn.classList.remove('not-visible');
@@ -13,4 +19,26 @@ reportBtn.addEventListener('click', () => {
   console.log("Event click intercepted");
   img.setAttribute('class', 'w-100');
   modalBody.prepend(img);
+  reportForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const formData = new FormData();
+    formData.append('csrfmiddlewaretoken', csrf);
+    formData.append('name', reportName.value);
+    formData.append('remarks', reportRemarks.value);
+    formData.append('image', img.src);
+
+    $.ajax({
+      type: 'POST',
+      url: '/reports/save/',
+      data: formData,
+      success: function(resp){
+        console.log(resp);
+      },
+      error: function(error) {
+        console.log(error);
+      },
+      processData: false,
+      contentType: false
+    });
+  })
 });
